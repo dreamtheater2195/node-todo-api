d36872dafac7354c7e95ea3facbaaecb10dc2c76
Quan Minh Pham <dreamtheater2195@gmail.com>
2018-05-03 23:45:17 +0700

    Add GET /api/users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
 var { ObjectID } = require('mongodb');
-
+var { authenticate } = require('./middleware/authenticate');
 var app = express();
 var port = process.env.PORT;
 
@@ -105,6 +105,11 @@ app.post('/api/users', (req, res) => {
     }).catch((e) => {
         res.status(400).send(e);
     });
+});
+
+
+app.get('/api/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 app.listen(port, () => {
